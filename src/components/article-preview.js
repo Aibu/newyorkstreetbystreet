bb0eb66876dde7a6efdb539c2c579b138a01c744
Francesco S <fsa@hellofresh.com>
2020-05-21 06:53:02 -0400

    Adds Youtube embed

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -6,7 +6,9 @@ import styles from './article-preview.module.css'
 
 export default ({ article }) => (
   <div className={styles.preview}>
-    <Img alt="" fluid={article.heroImage.fluid} />
+    <Link to={`/blog/${article.slug}`}>
+      <Img alt={article.title} fluid={article.heroImage.fluid} />
+    </Link>
     <h3 className={styles.previewTitle}>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h3>
@@ -17,7 +19,7 @@ export default ({ article }) => (
       }}
     />
     {article.tags &&
-      article.tags.map(tag => (
+      article.tags.map((tag) => (
         <p className={styles.tag} key={tag}>
           {tag}
         </p>
